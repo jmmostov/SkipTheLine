2f447c393530ca66783303e816cc858de78ed2da
rama96x <raak19ac@student.cbs.dk>
2020-03-30 10:10:01 +0200

    Næsten færdig login system med admin, linestander og customer.

--- controller/storeUser.js
+++ controller/storeUser.js
@@ -1,19 +1,21 @@
-const User = require('../models/User.js')
-const path = require('path')
+const User = require('../models/User.js');
+const path = require('path');
+
 
 module.exports = (req,res)=>{
-    console.log(req)
-    var newUser = new User({username: req.body.username, password: req.body.password, isAdmin: req.body.isAdmin});
-    User.create(req.body, (error,user)=>{
-        console.log(newUser.isAdmin)
-        if(req.body.adminCode === "secretCode") {
-            newUser.isAdmin = true;
-            console.log(newUser.isAdmin)
-            res.redirect('/')
-        }else {
-            newUser.isAdmin = false;
-            res.redirect('/')
-            console.log(newUser.isAdmin)
+    User.create({
+        username:req.body.username,
+        password: req.body.password,
+        userType: "customer",
+    }, (error,User)=>{
+        if(error) {
+            console.log(error)
+            return res.redirect('/register')
         }
+        else {
+            res.redirect('/login');
+            console.log(User)
+        }
+
     })
-}
+};
