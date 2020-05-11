ca96c6b6c6011923f2b570e4367b754124ba08d5
stinenorklitlonborg <stlo19ab@student.cbs.dk>
2020-05-11 13:06:08 +0200

    Kommentarer

--- controller/admin/getRegisterLinestander.js
+++ controller/admin/getRegisterLinestander.js
@@ -1,41 +1,18 @@
 var User = require('../../models/User');
-//Det eneste exec skulle, var at udføre linje 4 - jeg havde blot assignet User.find({...}) til en variabel 'info'
-// og derfor skal man skrive exec foran
-//var info = User.find({userType: 'linestander'});
 
-/*
-Alle userytype med typen 'linestander' bliver fundet og vist
-Jeg udkommenterede exec (execute) da denne kode gør præcis det samme!
- */
+//Creates a callfunction that finds all the users with the userType linestander.
 module.exports = async (req,res) =>{
-    //Er det en fejl at at I prøver at finde alle produkterne her? Produkterne bliver jo allerede fundet i adminProducts.js
-   // const products = await Product.find({})
+
     await User.find({userType: 'linestander'}, function (err,user) {
         if(err) {
             console.log('Fejlen er: ' + err)
         }
+        //If the admin is logged in then it should response with the registerLinestander.ejs page.
         if(admin) {
             res.render('registerLinestander', {
                 users: user,
-              //  products
             })
         }
         else return res.redirect('/error')
     })
 }
-/*
-module.exports = async (req,res)=>{
-    const products = await Product.find({});
-    await info.exec(function(err,user) {
-        if(err) {
-            console.log(err)
-        }
-        if(admin){
-            res.render('registerLinestander',{
-                users: user,
-                products
-            });
-        }
-        else return res.redirect('/error');
-    });
-};*/
--- controller/admin/updateDataForAdmin.js
+++ controller/admin/updateDataForAdmin.js
@@ -1,23 +1,10 @@
-//update username via objectId that has been created by mongoDB
 
-
-/*
-module.exports = function (req,res) {
-    User.findByIdAndUpdate({_id: req.params.id}, req.body).then(function (user) {
-        User.findOne({_id:req.params.id}).then(function (user) {
-            res.send(user)
-        })
-    });
-}
- */
 let User = require('../../models/User');
 
 module.exports = function(req,res) {
     console.log(req.body)
 
     User.findOneAndUpdate(
-        // Vi kan ikke komme ind og få fat på værdierne i vores ejs ved at bruge "req.body..."
-        // https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
         {
             _id: req.body.id
         },
@@ -39,39 +26,6 @@ module.exports = function(req,res) {
         })
         .catch(error => console.error(error))
 }
-
-/*
-     await User.findOneAndUpdate({_id: req.body.id},{$set:{username:req.body.username}},{new: true}, function(err,user){
-        if(err){
-            console.log('errormessage ' + err);
-        }
-        //res.send(user);
-        return res.redirect(303, '/registerLinestander')
-    });
-};
-
- */
-
-
-
-
-
-/*module.exports = (req,res) => {
-    console.log('test 1 ')
-    const formUpdate = req.body.formButton;
-   // var updateForm = req.getElementById('updateForm');
-    console.log('test 2 ')
-    formUpdate.onsubmit = (event) => {
-        event.preventDefault();
-
-        fetch('registerLinestander', {
-            method: 'PUT',
-            body: new FormData(formUpdate)
-        }).then(response => response.json())
-            .then(json => console.log(json))
-    }
-};
-*/
 
 
 
